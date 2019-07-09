9b87aa1dc84f0f941984abc8709fc5b2161545a6
yunustr <ynsture@gmail.com>
2019-07-10 00:47:27 +0300

    comments made for development use

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
+///REQUİREMENTS ///
 const    express =        require("express"),
         mongoose =        require ("mongoose"),
        bodyParser=        require ("body-parser"),
         Exercise =        require("./models/exercise"),
              User=        require("./models/user");
-             
-
-                       require('dotenv').config();
+                          require('dotenv').config();
         
 
 const app=express();
 
-
+//PORT 
 const port=process.env.PORT||3000;
-
+//CONFİGS
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true });
@@ -21,20 +20,21 @@ mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true });
 
 
 
-
+////////////////ROUTES//////////////////
+// 1) GET THE MAİN PAGE  /////
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/views/index.html");
 });
 
-
+// CREATE A NEW USER  /////
 app.post("/api/exercise/new-user",(req,res)=>{
     let name=req.body.username;
    User.findOne({"name":name},"-exercise",(err,foundedUser)=>{
-        if(err){
+        if(err){                                                    //CHECK IF THERE IS A USER WITH THIS NAME OR NOT
            res.send(err)
         }else {
-           if(foundedUser){
+           if(foundedUser){                                           //IF USER SAVED SEND THE PARAMATERS 
              res.json({name:name,id:foundedUser._id ,status:"saved user"})
            }else{
             let newUser={"name":name};
@@ -42,7 +42,7 @@ app.post("/api/exercise/new-user",(req,res)=>{
                 if(err){
                     res.json(err)
                 }else{
-                   res.json({name:name,id:createdUser._id,status:"new Created User"})
+                   res.json({name:name,id:createdUser._id,status:"new Created User"}) // USER CREATED 
                 }
             })
                
@@ -57,12 +57,7 @@ app.post("/api/exercise/new-user",(req,res)=>{
       let date=req.body.date;
       let name=req.body.exercisename;
       let userid=req.body.userid;
-     
-          
-      
-      
-   
-    User.findById(userid,(err,data)=>{
+ User.findById(userid,(err,data)=>{
         if(err){
            res.send(err)
         }else{
